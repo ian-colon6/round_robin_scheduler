1376a19f34f7f2f632ddf3f667526a983cb91e46
Ián Colón <ian.colon6@upr.edu>
2021-10-07 17:55:17 -0400

    first version of RR implementation

--- src/main/java/prj_01/RoundRobinCLL.java
+++ src/main/java/prj_01/RoundRobinCLL.java
@@ -1,4 +1,5 @@
 package prj_01;
+import java.util.LinkedList;
 import java.util.concurrent.ThreadLocalRandom;
 
 
@@ -25,6 +26,7 @@ public class RoundRobinCLL implements RoundRobinCLLInterface {
     public Node tail = null;
     public Boolean stopLoop = false;
     private int termination_limit;
+    public LinkedList<Node> processes = new LinkedList<Node>();
 
     private void holdon() {
         try{
@@ -64,6 +66,17 @@ public class RoundRobinCLL implements RoundRobinCLLInterface {
         /* STARTING FROM THE FIRST NODE IN THE LINKED LIST */
         /*** IMPORTANT:: USE THE holdRR() METHODE TO ACCESS THE LINKED LIST ***/
         /*** TO AVOID RACE CONDITION ***/
+        int count = 0;
+        while(!stopLoop){
+
+            if(count > termination_limit) break;
+
+            if(processes.get(count).proccessed_flag == false){
+                holdRR(processes.get(count + 1), true);
+            }
+
+            count++;
+        }
     }
 
     public void findFilledSlot() {
@@ -74,13 +87,34 @@ public class RoundRobinCLL implements RoundRobinCLLInterface {
         while (!stopLoop) {
             /* PUT YOUR CODE HERE TO FIND THE FILLED SLOTS */
             if (count>termination_limit) break;
+
+            if( processes.get(count).proccessed_flag ){
+                holdRR(processes.get(count + 1), false);
+            }
+
             System.out.println("Main Move No.: " + count%num_nodes + "\t" + toString());
+            count++;
         }
     }
 
     private void fillRoundRubin () {
         /* PUT YOUR CODE HERE INITIATE THE CIRCULAR LINKED LIST */
         /* WITH DESIRED NUMBER OF NODES BASED TO THE PROGRAM   */
+
+        for(int i = 0; i < num_nodes; i++){
+
+            Node proNode = new Node(i);
+            if(head == null){
+                head = proNode;
+            }else{
+                tail.next = proNode;
+            }
+
+            tail = proNode;
+            tail.next = head;
+
+            processes.add(proNode);
+        }
     }
 
     public RoundRobinCLL(int num_nodes, int termination_limit) {
